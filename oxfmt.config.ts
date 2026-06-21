0d2c74a29ae2767446686c955d7e02da071be57c
4leite <4leite@gmail.com>
2026-06-21 23:29:20 +1200

    reference site

--- oxfmt.config.ts
+++ oxfmt.config.ts
@@ -4,6 +4,6 @@ export default defineConfig({
   semi: false,
   trailingComma: "all",
   proseWrap: "always",
-  ignorePatterns: ["src/routeTree.gen.ts", "*.d.ts"],
+  ignorePatterns: ["src/routeTree.gen.ts", "*.d.ts", "reference-site/**/*"],
   sortImports: true,
 })
--- oxlint.config.ts
+++ oxlint.config.ts
@@ -18,6 +18,7 @@ export default defineConfig({
     ".tanstack/**/*",
     ".playwright/**/*",
     "node_modules/**/*",
+    "reference-site/**/*",
     "dist/**/*",
     ".rollup.cache/**/*",
     ".playwright/e2e-runtime/**",
